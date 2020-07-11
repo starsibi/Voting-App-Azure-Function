1bfadcc49361ce8e025f17d7d8fe0be7b4f56489
starsibi <starsibi@gmail.com>
2020-07-11 18:00:28 +0530

    Initial Changes

--- Voting App/AddVote.cs
+++ Voting App/AddVote.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
-using Microsoft.WindowsAzure.Storage;
 using System;
 using Microsoft.AspNetCore.Mvc;
 
